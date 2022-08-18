bcbb6abe17adc0316df01e512709a59f835efd14
Borhen Benltaief <borhen.benletaief@gmail.com>
2022-08-18 17:18:10 +0100

    modified:   clientside/package-lock.json 	modified:   clientside/package.json 	deleted:    clientside/public/logo192.png 	deleted:    clientside/public/logo512.png 	modified:   clientside/src/App.tsx 	modified:   node_modules/.package-lock.json 	modified:   package-lock.json 	modified:   package.json 	modified:   server/database.sql 	modified:   server/node_modules/.bin/esparse 	modified:   server/node_modules/.bin/esparse.cmd 	deleted:    server/node_modules/.bin/esparse.ps1 	modified:   server/node_modules/.bin/esvalidate

--- clientside/src/App.tsx
+++ clientside/src/App.tsx
@@ -1,14 +1,28 @@
-import React, { Fragment } from "react";
+import React from "react";
 import "./App.css";
 
-import InputTodo from "./components/inputTodo";
-import allTodos from "./components/allTodos";
+/*import InputTodo from "./components/inputTodo";
+import allTodos from "./components/allTodos";*/
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Login/index";
+import Register from "./components/Register/index";
+import Home from "./components/Home/index";
 function App() {
   return (
+    /*
     <Fragment>
       <div className="container mt-5 text-center">{InputTodo()}</div>
       <div className="container mt-5 text-center">{allTodos()}</div>
     </Fragment>
+    */
+    <BrowserRouter>
+      <Routes>
+        {/* <Route path="/" element={<Login />} /> */}
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- server/database.sql
+++ server/database.sql
@@ -4,3 +4,12 @@ CREATE TABLE todo(
     todo_id SERIAL PRIMARY KEY,
     description VARCHAR(255)
 );
+
+CREATE TABLE users(
+    user_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    username VARCHAR(255),
+    email VARCHAR(255),
+    password VARCHAR(255)
+);
